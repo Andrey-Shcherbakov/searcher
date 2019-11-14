3d03964f3e2bd53324e5db86788bb3247b61e071
Andrey-Shcherbakov <scherbakovan27@mail.ru>
2019-11-14 11:51:48 +0300

    final.

--- akm.cpp
+++ akm.cpp
@@ -10,8 +10,8 @@ akm:: akm(const std:: string &pattern):s (pattern),
 }
 
 int akm:: step(int q, char input) {
-    assert((int)((unsigned char) input) < 256 && (int)((unsigned char) input) > 0);
-    assert(q > 0 && q <= s.length());
+    assert((int)((unsigned char) input) < 256 && (int)((unsigned char) input) >= 0);
+    assert(q >= 0 && q <= s.length());
     return table[q][(int)((unsigned char) input)];
 }
 
@@ -27,19 +27,21 @@ void akm:: AKM (){
             else table[q][a] = table[pf[q-1]][a];
         }
     }
-} 
+}
 
 void akm:: pff(){
-    pf[0] = 0;
-	for (int k = 0, i = 1; i < s.length(); ++i) 
+  pf[0] = 0;
+	for (int k = 0, i = 1; i < s.length(); ++i)
 	{
-		while ((k > 0) && (s[i] != s[k]))
+		while ((k > 0) && (s[i] != s[k])){
 			k = pf[k-1];
+    }
 
-		if (s[i] == s[k])
+		if (s[i] == s[k]){
 			k++;
+    }
 
-		assert(k < s.length() && k > 0);
+		assert(k <= s.length() && k >= 0);
 		pf[i] = k;
 	}
 }
